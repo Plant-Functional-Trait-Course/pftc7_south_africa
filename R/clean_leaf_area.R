ceb7758cbaee71061e5c0fa7a2f6e88cf73d4857
audhalbritter <me@audhalbritter.com>
2023-12-16 10:16:46 +0200

    add leaf area data

---
+++ R/clean_leaf_area.R
@@ -0,0 +1,21 @@
+# CLEAN LEAF AREA
+
+# download trait data from OSF
+get_file(node = "hk2cy",
+         file = "PFTC7_leaf_area_2023.csv",
+         path = "raw_data",
+         remote_path = "raw_data/raw_trait_data")
+
+raw_area <- read_csv("raw_data/PFTC7_leaf_area_2023.csv") |>
+  clean_names() |>
+  mutate(id = case_when(id == "mIWJ2357" ~ "IWJ2357",
+                        id == "IMU9111I" ~ "IMU9111",
+                        id ~ TRUE))
+
+# raw_area |>
+#   anti_join(valid_codes, by = c("id" = "hashcode"))
+
+
+raw_area |>
+  ggplot(aes(x = leaf_area)) +
+  geom_histogram()
