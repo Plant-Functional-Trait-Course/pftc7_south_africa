e27a3972d0916f5a6aeea5e59346effcb21946c1
Imke Smit <153980493+ImkeSmit@users.noreply.github.com>
2024-04-12 10:24:07 +0200

    Update Spname_fixes.R
    Downloading the new helichrysum naming system from OSF rather than Google drive. 
    Also changing the step 2 loop so that it can be applied to more than one dataframe, in this case to the trait data and community data.

--- R/sp_name_changes/Spname_fixes.R
+++ R/sp_name_changes/Spname_fixes.R
@@ -63,36 +63,67 @@ rm(correct_name, tochange, old_name, tochange_row, i)
 
 #### Step 2 - change the naming system for Helichrysum ####
 
+#download the new naming system from OSF
+get_file(node = "hk2cy",
+         file = "New Helichrysum naming system.xlsx",
+         path = "raw_data",
+         remote_path = "raw_data/raw_community_data")
+#import new naming system
+naming_system <- read_excel("raw_data/New Helichrysum naming system.xlsx")
+
+#import other data which also needs names changed to the new naming system
+get_file(node = "hk2cy",
+         file = "PFTC_SA_clean_community_2023.csv",
+         path = "clean_data",
+         remote_path = "community_data")
+#import clean community data
+comm_step_2 <- read.csv("clean_data/PFTC_SA_clean_community_2023.csv")
+
 FT_step_2 <- FT_step_1 #create another copy for step 2 (changing Helichrysum names according to the new naming system)
 
-naming_system <- read_excel("R/sp_name_changes/Heli_naming_system.xlsx")
+#create a list of dataframes to apply the new naming system to
+datalist <- c("FT_step_2", "comm_step_2")
+#a vector of names to assign to the data once the new naming system has been applied
+finished_list <- c("FT_new_name_system", "comm_new_name_system")
 
-for (i in 1:nrow(FT_step_2)) {
-  old_name <- FT_step_1[i, 6]
-  new_name <- NA
+for(d in 1:length(datalist)) {
 
-  found <- FALSE
-  for (j in 1:nrow(naming_system)) { # looks whether species name should be corrected and replaces it with the new_heli_name_system in case
-    found <- grepl(old_name, naming_system[j, 1])
+  data <- get(datalist[d])
+  #create a column to store the name changes in
+  data$name_changes <- NA
 
-    if (is.na(found)){ # only runs if the species is missing
-      found <- FALSE
+  for (i in 1:nrow(data)) {
+    old_name <- data[i, which(colnames(data) == "species")]
+    new_name <- NA
+
+    found <- FALSE
+    for (j in 1:nrow(naming_system)) { # looks whether species name should be corrected and replaces it with the new_heli_name_system in case
+      found <- grepl(old_name, naming_system[j, 1])
+
+      if (is.na(found)){ # only runs if the species is missing
+        found <- FALSE
+      }
+
+      if (found){ # only runs if the species is a synonym
+        new_name <- naming_system[j, 2] # finds the true name of the species and saves it
+        break
+      }
     }
 
-    if (found){ # only runs if the species is a synonym
-      new_name <- naming_system[j, 2] # finds the true name of the species and saves it
-      break
-    }
-  }
+    if (found) { # replaces the species in the trait database with the saved true name if "found" is "TRUE"
+      data[i, which(colnames(data) == "species")] <- new_name
+      data[i, which(colnames(data) == "name_changes")] <- paste0("name changed from ", old_name, " to ", new_name)
 
-  if (found) { # replaces the species in the trait database with the saved true name if "found" is "TRUE"
-    FT_step_2[i, 6] <- new_name
-    FT_step_2[i, 17] <- paste0("name changed from ", old_name, " to ", new_name)
-  }
-}
+      #assign a new name to data
+      assign(finished_list[d], data)
+
+    }
+  }#end loop through rows
+}#end loop through dataframes
 
 rm(old_name, new_name, found, i, j) # remove unnecessary variables
 
-write.csv(FT_step_2, "clean_data/PFTC7_SA_clean_traits_2023.csv") # write a new file with the finalized data
+write.csv(FT_new_name_system, "clean_data/PFTC7_SA_clean_traits_2023.csv") # write a new file with the finalized data
+write.csv(comm_new_name_system, "clean_data/PFTS_SA_clean_community.csv")
 
 # since this should be the final step in the data cleaning process data will be save in clean_data
