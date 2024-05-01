89f3a2c00475aec44390c9ee5dc1c420ebccf8b2
ImkeSmit <imke610@gmail.com>
2024-05-01 10:35:35 +0200

    The double species entries have been fixed, so delete that code. make comm wide and replace NA with 0

--- R/community figures.R
+++ R/community figures.R
@@ -20,27 +20,12 @@ comm_wide <- comm |>
   column_to_rownames(var = "plotref")
 comm_wide <- as.data.frame(comm_wide)
 
-
-comm |>
-  filter(is.na(treatment_only_for_range_x)) |> #only work with site 1-5, not rangex site
-  mutate(plotref = paste(site_id, aspect, plot_id, sep = "_")) |>
-  select(plotref, species, cover) |>
-  group_by(plotref, species) |>
-  summarise(n = n(), .groups = "drop") |>
-  filter(n > 1L)
-
-comm |>
-  mutate(plotref = paste(site_id, aspect, plot_id, sep = "_")) |>
-  filter(plotref == "2_E_4") |>
-  select(species) |>
-  arrange(species)
-
-#there must be multiple entries of the same species in a plot causing the list
-
-#replace NULL values with 0
+#replace NA values with 0
 for(r in 1:nrow(comm_wide)) {
   for(c in 1:ncol(comm_wide)) {
-    if(comm_wide[r,c])
+    if(is.na(comm_wide[r,c])) {
+      comm_wide[r,c] <- 0
+    }
   }
 }
 
