2712250b687f3f801e84a9f6faee67c2332087b6
ImkeSmit <imke610@gmail.com>
2024-05-01 11:18:49 +0200

    Make a plot

--- R/community figures.R
+++ R/community figures.R
@@ -2,10 +2,18 @@
 library(tidyverse)
 library(tidylog)
 library(ggplot2)
+library(RColorBrewer)
 library(vegan)
 
 #import community data
-comm <- read.csv("clean_data/PFTC7_SA_clean_community_19Apr2024.csv", row.names = 1)
+comm <- read.csv("clean_data/PFTC7_SA_clean_community_19Apr2024.csv", row.names = 1) |>
+  mutate(elevation = case_when(site_id == 1 ~ 2000,
+                               site_id == 2 ~ 2200,
+                               site_id == 3 ~ 2400,
+                               site_id == 4 ~ 2600,
+                               site_id == 5 ~ 2800,
+                               site_id == 6 ~ 3000))
+
 comm$site_id <- as.factor(comm$site_id)
 comm$aspect <- as.factor(comm$aspect)
 comm$plot_id <- as.factor(as.numeric(comm$plot_id))
@@ -30,3 +38,29 @@ for(r in 1:nrow(comm_wide)) {
 }
 
 
+##Now we can make the nmds
+set.seed(10)
+ord <-metaMDS(comm_wide, distance = "bray", k = 3)
+
+#get the site scores
+site_scores <- as.data.frame(scores(ord, display = "sites"))
+#add the elevation and aspect to the sites_scores dataframe
+siteinfo <- comm |>
+  filter(is.na(treatment_only_for_range_x)) |> #remove rangex records
+  distinct(site_id, elevation, aspect, plot_id) |>
+  mutate(plotref = paste(site_id, aspect, plot_id, sep = "_"))
+
+site_scores_join <- site_scores |>
+  rownames_to_column(var = "plotref") |>
+  left_join(siteinfo, by = "plotref")
+site_scores_join$elevation <- as.factor(site_scores_join$elevation)
+
+pal <- brewer.pal(5, "Set2")
+
+##now create the ordination plot
+ggplot(site_scores_join, aes(x = NMDS1, y = NMDS2, color = elevation, shape = aspect)) +
+  geom_point() +
+  theme_classic() +
+  scale_color_manual(values = c(pal)) +
+  scale_shape_manual(labels = c("East", "West"), values = c(1, 16)) +
+  labs(color = "Elevation (m.a.s.l.)", shape = "Aspect")
