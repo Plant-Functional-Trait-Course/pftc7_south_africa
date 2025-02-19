ea89b7a380855c205283014d559e44a542ac7689
audhalbritter <me@audhalbritter.com>
2025-02-19 14:47:09 +0100

    standardize biomass per plot

--- R/functions/clean_biomass.R
+++ R/functions/clean_biomass.R
@@ -10,7 +10,9 @@ clean_biomass <- function(raw_biomass){
     # summarize biomass that was in one bag
     group_by(site_id, aspect, plot_id, functional_group) |>
     tidylog::summarise(biomass_g = mean(biomass_g)) |>
-    ungroup()
+    ungroup() |>
+    # biomass is from an area of 0.12 m2 (3 x 20cm x 20cm)
+    # needs transforming to plot size 1.44 m2 (1.2m x 1.2m)
+    mutate(biomass_g = 1.44 * biomass_g / 0.12)
 
 }
-
