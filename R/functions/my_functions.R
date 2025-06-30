6d580dd131cc4f69810fd2acd4372266623bc56f
audhalbritter <me@audhalbritter.com>
2025-06-30 15:11:27 +0200

    add nr to datasets

--- R/functions/my_functions.R
+++ R/functions/my_functions.R
@@ -1,9 +1,9 @@
 # other functions
 
 # save data as csv
-save_csv <- function(file, name) {
+save_csv <- function(file, nr, name) {
 
-  filepath <- paste0("clean_data/", "PFTC7_SA_clean_", name, ".csv")
+  filepath <- paste0("clean_data/", nr, "_", "PFTC7_clean_", name, ".csv")
   output <- write_csv(x = file, file = filepath)
   filepath
 }
